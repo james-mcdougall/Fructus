b53f9c8266025ed41827df5e3a4f07c7752d3ba7
james-mcdougall <jameswilliammcdougall@gmail.com>
2023-09-05 15:52:53 -0700

    Initial Project Commit

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by James McDougall on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
